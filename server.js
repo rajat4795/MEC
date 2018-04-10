73d3d30da72a06fe932cef333d567c3b5efee761
Rajat Chaudhari <36498393+rajat4795@users.noreply.github.com>
2018-04-10 18:31:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require ('express');
 var app = express();
 var fs= require ('fs');
-var id=2;
 var redis=require ('redis');
 var publisher = redis.createClient();
 var subscriber = redis.createClient();
@@ -22,75 +21,154 @@ var port= server.address().port
 
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({extended:false}))
+	
 
 	app.post('/serviceList', function(req,res){
-		//console.log(typeof(req.body.reset));
-
+		
 		var data = JSON.stringify(req.body.reset);
-
 		client.set('reset',data);
 		publisher.publish("serviceList",data);
 		console.log("Client made a RESET");
 		console.log(data);
-		//res.sendStatus(200);
-		res.end("Reset Done!!");
+		res.end("Reset Done!!");	
+		})
 		
-	})
-
 	app.post('/saveList',function(req,res){
 		var data = JSON.stringify(req.body.services);
 		client.set('services',data);
 		publisher.publish("saveList",data);
 		console.log("List of services saved");
 		console.log(data);
-		//res.sendStatus(200);
 		res.end("Services SAVED!!");
 		
 	})
 
 	app.post('/serviceConfig',function(req,res){
-
-		var data = JSON.stringify(req.body.config);
-		client.set('config',data);
-		client.set('stats',data);
+		var vlan=req.body["config"]["vlan"][0].Id;
+		//console.log('config'+vlan);
+		console.log(vlan);
+		if(vlan==5){
+			var action=req.body["config"].action;
+		console.log(action);
+		if(action == 'add')
+		{
+			var data = JSON.stringify(req.body.config);
+		client.set('config5',data);
 		publisher.publish("serviceConfig",data);
-		client.get('config', function(err, reply){
-			fs.writeFile(__dirname+"/"+"configUI.json",reply,function(err){
-			if(err){
-			console.log(err);
-			res.sendStatus(404);	
-					}else
-					{
-						console.log('successfully added json data');
-						console.log(reply);
-						//res.sendStatus(200);
-						res.end("Configuration SAVED!!");
-					}			
-			});
+		console.log('successfully added json data');
+		console.log(data);
+		res.end("Configuration for vlan 5 SAVED!!");	
+		}
+			
+		 if(action == 'delete'){
+			client.del('config5');
+			publisher.publish("serviceConfig","Configuration deleted");
+			console.log('configuration for vlan 5 DELETED!!');
+			res.end('config5 deleted');
+		}
+	}
+		if(vlan==6){
+			var action=req.body["config"].action;
+		console.log(action);
+		if(action == 'add')
+		{
+			var data = JSON.stringify(req.body.config);
+		client.set('config6',data);
+		publisher.publish("serviceConfig",data);
+		console.log('successfully added json data');
+		console.log(data);
+		res.end("Configuration for vlan 6 SAVED!!");	
+		}
+			
+		 if(action == 'delete'){
+			client.del('config6');
+			publisher.publish("serviceConfig","Configuration deleted");
+			console.log('configuration for vlan 6 DELETED!!')
+			res.end('config6 deleted');
+		}
+	}
+		
 	
-		});
+		})
 
-	})
+	
 
 	app.get('/stats',function(req,res){
-		//subscriber.subscribe("stats");
 		client.get('stats', function(err, data){
-			fs.writeFile(__dirname+"/"+"statsUI.json",data,function(err){
-				if(err){
-					console.log(err);
-					res.sendStatus(404);
-				}else{
-					console.log("successfully sent json data");
+			if(err){
+				throw err;
+			}else{
+				console.log("successfully sent json data");
 					console.log(data);
 					res.end(data);
+			}
 					
-				}
 			});
-		
-		});
 
 	})
 
+	app.get('/serviceList',function(req,res){
+		client.get('reset',function(err,data){
+			if(err){
+				throw err;
+			}else{
+			console.log('Reset data stored in Redis DB');
+			console.log(data);
+			res.end(data);
+		}
+		});
+	})
+
+	app.get('/saveList',function(req,res){
+		client.get('services',function(err,data){
+			if(err){
+				throw err;
+			}else{
+			console.log('Service list data stored in Redis DB');
+			console.log(data);
+			res.end(data);
+			}
+			
+		});
+	})
+
+	app.get('/serviceConfig',function(req,res){
+		client.get('config5',function(err,data){
+			if(err){
+				throw err;
+			}else{
+			console.log('Configuration data stored in Redis DB');
+			console.log(data);
+			res.end(data);
+		}
+		});
+	})
+
+/*var action=req.body["config"].action;
+		console.log(action);
+		if(action == 'add')
+		{
+			var data = JSON.stringify(req.body.config);
+		client.set('config',data);
+		publisher.publish("serviceConfig",data);
+		console.log('successfully added json data');
+		res.end("Configuration SAVED!!");	
+		}
+			
+		 if(action == 'delete'){
+			client.del('config');
+			publisher.publish("serviceConfig","Configuration deleted");
+			console.log('configuration deleted')
+			res.end('config deleted');
+		}*/	
+
+
+
+/*subscriber.subscribe("stats");
+		subscriber.on('message',function(channel,message){
+			console.log(channel,message);
+			// /client.set('stats',message);
+			res.end(message);*/
 	
 
 
