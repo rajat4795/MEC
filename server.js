cb05ef54b765770e10fa34036b702bc701eabb1b
Rajat Chaudhari <36498393+rajat4795@users.noreply.github.com>
2018-04-12 09:58:27 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var express = require ('express');
 var app = express();
-//var fs= require ('fs');
 var redis=require ('redis');
 var publisher = redis.createClient();
-//var subscriber = redis.createClient();
 var bodyParser =require('body-parser');
 var util =require('util');
 var client = redis.createClient();
+var scanner =require('redis-scanner');
+scanner.bindScanners(client);
 
 
 var server =app.listen(8081,function(){
 var port= server.address().port
-		console.log("listening at port localhost:%s",port)
+var host= server.address().address
+		console.log("listening at port %s:%s",host,port)
 	})
 		client.on('connect',function(){
 		console.log('Connected to redis');
@@ -25,7 +26,7 @@ var port= server.address().port
 		
 		var data = JSON.stringify(req.body);
 		client.set('reset',data);
-		publisher.publish("serviceList",data);
+		publisher.publish("mgmt",data);
 		console.log("Client made a RESET");
 		console.log(data);
 		res.end("Reset Done!!");	
@@ -34,7 +35,7 @@ var port= server.address().port
 	app.post('/saveList',function(req,res){
 		var data = JSON.stringify(req.body);
 		client.set('services',data);
-		publisher.publish("saveList",data);
+		publisher.publish("mgmt",data);
 		console.log("List of services saved");
 		console.log(data);
 		res.end("Services SAVED!!");
@@ -48,10 +49,11 @@ var port= server.address().port
 		console.log(action);
 		if(action == 'add')
 		{
-			//console.log(JSON.stringify(req.body));
 		var data = JSON.stringify(req.body);
 		client.set('config'+vlan,data);
-		publisher.publish("serviceConfig",data);
+		publisher.publish("fw",data);
+		publisher.publish("dns",data);
+		publisher.publish("mgmt",data);
 		var msg= util.format('Configuration for vlan %s SAVED!',vlan);
 		console.log(msg);
 		console.log(data);
@@ -59,8 +61,14 @@ var port= server.address().port
 		}
 			
 		 if(action == 'delete'){
+			var data=client.get('congig'+vlan);
+			/*publisher.publish("fw","Configuration deleted");
+			publisher.publish("fw",data);
+			publisher.publish("dns","Configuration deleted");
+			publisher.publish("dns",data);
+			publisher.publish("mgmt","Configuration deleted");
+			publisher.publish("mgmt",data);*/
 			client.del('config'+vlan);
-			publisher.publish("serviceConfig","Configuration deleted");
 			var msg=util.format('configuration for vlan %s DELETED!!',vlan)
 			console.log(msg);
 			res.end(msg);
@@ -68,23 +76,24 @@ var port= server.address().port
 		
 		})
 
-	
+	app.post('/stats',function(req,res){
+		var data = JSON.stringify(req.body);
+		publisher.publish("stats",data);
+		client.set('stats',data);
+		console.log("stats saved");
+		res.end("stats saved");
+	})
 
 	app.get('/stats',function(req,res){
-		
 		client.get('stats', function(err, data){
 			if(err){
 				throw err;
 			}else{
 				console.log("successfully sent json data");
-
 					console.log(data);
 					res.end(data);
-			}
-					
+			}		
 			});	
-		
-
 	})
 
 	app.get('/serviceList',function(req,res){
@@ -112,24 +121,52 @@ var port= server.address().port
 		});
 	})
 
+
 	app.get('/serviceConfig',function(req,res){
-		client.get('config5',function(err,data){
-			if(err){
-				throw err;
-			}else{
+	var file=[];
+	client.keys('config*',function(err,keys){
+		if(err)
+			return console.log(err);
+		keys.forEach(function(reply,index){
+			client.get(reply,function(err,data){
+				var resData =JSON.parse(data);
+				for(var key in resData){
+					file.push(resData[key]);	
+				}
+				console.log(file);
+				client.set('allconfig',JSON.stringify(file));
+			});
+			
+		})
+			});
+		
+		client.get('allconfig',function(err,data){
+			var Data= '{"config":'+data+"}";
+			res.end(Data);
+		});		
+		})
+
+
+
+
+	/*app.get('/serviceConfig',function(req,res){
+		var resData;
+		for(var i=1;i<=10;i++){
+			var config= 'config'+i;
+			//console.log(config);
+			client.get(config,function(err,data){
+			resData=resData+data;
 			console.log('Configuration data stored in Redis DB');
 			console.log(data);
-			res.end(data);
-		}
+			
 		});
-	})
+		
+			res.end(resData);
+		
+		}
+	})*/
 
-	app.post('/a',function(req,res){
-		var data = JSON.stringify(req.body);
-		publisher.publish("stats",data);
-		client.set('stats',data);
-		res.end();
-	})
+
 
 	/*if(vlan==5){
 			var action=req.body["config"].action;
---
+++ serverstart.sh
@@ -0,0 +1,6 @@
+sudo apt install nodejs-legacy -y
+sudo apt install npm -y
+npm install
+sudo apt install redis-server
+sudo apt-get update
+npm start
